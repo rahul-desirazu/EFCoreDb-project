26482154cff689dde6d9aca1b94ee8533a0684db
james <jmoak106@gmail.com>
2023-03-03 11:45:55 +0100

    feat: added hashset to models and some commenting

--- EFCoreDB/Models/Character.cs
+++ EFCoreDB/Models/Character.cs
@@ -3,24 +3,35 @@ using System.ComponentModel.DataAnnotations;
 
 public class Character
 {
+
+    public Character()
+    {
+        Movies = new HashSet<Movie>();
+    }
+
+    // The ID of the movie entity as Primary Key.
     [Key]
     public int CharacterId { get; set; }
 
+    // The name of the character.
     [Required]
     [StringLength(50)]
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
+    // The characters Alias.
     [Required]
     [StringLength(50)]
-    public string Alias { get; set; }
+    public string Alias { get; set; } = null!;
 
+    // The Gender of a character as three choices.
     [Required]
-    [StringLength(50)]
-    public string Gender { get; set; }
+    [RegularExpression("^(Male|Female|Other)$")]
+    public string Gender { get; set; } = null!;
 
+    // The URL of a character as three choices.
     [Required]
     [StringLength(200)]
-    public string PictureUrl { get; set; }
+    public string PictureUrl { get; set; } = null!;
 
     //Navigational properties between Character and Movies
     public virtual int MovieId { get; set; }
--- EFCoreDB/Models/Movie.cs
+++ EFCoreDB/Models/Movie.cs
@@ -3,6 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 public class Movie
 {
+    public Movie()
+    {
+        Characters = new HashSet<Character>();
+
+    }
+
     // The ID of the movie entity as Primary Key.
     [Key]
     public int MovieId { get; set; }
@@ -10,36 +16,36 @@ public class Movie
     // The Title of the movie.
     [Required]
     [StringLength(50)]
-    public string Title { get; set; }
+    public string Title { get; set; } = null!;
 
     // The genre of the movie.
     [Required]
     [StringLength(50)]
-    public string Genre { get; set; }
+    public string Genre { get; set; } = null!;
 
     // The year the movie was released.
     [Required]
     [Range(1900, 2023)]
-    public string ReleaseYear { get; set; }
+    public string ReleaseYear { get; set; } = null!;
 
     // Director of the movie.
     [Required]
     [StringLength(50)]
-    public string Director { get; set; }
+    public string Director { get; set; } = null!;
 
     // The URL of the picture for the movie.
     [Required]
     [StringLength(200)]
-    public string PictureUrl { get; set; }
+    public string PictureUrl { get; set; } = null!;
 
     // The URL of the trailer for the movie.
     [Required]
     [StringLength(200)]
-    public string TrailerUrl { get; set; }
+    public string TrailerUrl { get; set; } = null!;
 
     // Navigation properties to be able to navigate to Franchise (Example on Modelbuilder in MyDBContext Class
     public int FranchiseId { get; set; }
-    public virtual Franchise Franchise { get; set; }
+    public virtual Franchise Franchise { get; set; } = null!;
     public virtual ICollection<Character>? Characters { get; set; }
 
 }

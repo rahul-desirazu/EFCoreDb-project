32c118be1aa0201b8c88a1bcdd85e4a1ded252b0
james <jmoak106@gmail.com>
2023-03-03 11:48:01 +0100

    Chore: Adding implemention to Profiles with automapper....

--- EFCoreDB/Profiles/CharacterProfile.cs
+++ EFCoreDB/Profiles/CharacterProfile.cs
@@ -1,8 +1,18 @@
 ﻿using AutoMapper;
+using EFCoreDB.Models;
+using EFCoreDB.Models.DTOs;
 
-namespace EFCoreDB.Profiles
+namespace YourProjectName.Profiles
 {
     public class CharacterProfile : Profile
     {
+        public CharacterProfile()
+        {
+            CreateMap<Character, CharacterCreateDto>()
+                .ForMember(dest => dest.MovieIds, opt => opt.MapFrom(src => src.Movies.Select(m => m.MovieId)));
+            CreateMap<CharacterForCreationDTO, Character>();
+            CreateMap<CharacterForUpdateDTO, Character>();
+            CreateMap<Character, CharacterForUpdateDTO>();
+        }
     }
 }

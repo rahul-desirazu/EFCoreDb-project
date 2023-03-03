6fe031ddc93f4e7577998acd3b1aa69a091aa7ad
james <jmoak106@gmail.com>
2023-03-03 10:13:34 +0100

    feat: made some exceptions and added in throws in services

---
+++ EFCoreDB/Util/Exeptions/CharacterNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreDB.Util.Exeptions
+{
+    public class CharacterNotFoundException : EntityNotFoundException
+    {
+        public CharacterNotFoundException() : base("Character not found with that Id.")
+        {
+        }
+
+    }
+}
